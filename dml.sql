1c3211c371128865b5578180dea1a8fc9aa40717
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2019-11-28 13:23:04 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+INSERT INTO TABLE_NAME (FIELD_NAME_1, FIELD_NAME_2) VALUES (value_1, value_2);
 -- Revise record
-
+UPDATE TABLE_NAME SET FIELD_NAME_1 = value_1;
 --  Delete record
-
+DELETE FROM TABLE_NAME WHERE FIELD_NAME_1 = value_1;
 -- Search record
+SELETE * FROM TABLE_NAME WHERE FIELD_NAME_1 = value_1;
